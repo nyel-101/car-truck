1c5d9f916dbe12309bceac33d7b6fa392418cbcf
nyel-101 <dandevjr@gmail.com>
2025-02-23 21:06:50 +0000

    fix(core): refactor database index

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62d19c9b538e48f6903264fdaffca190 = () => {
-  return '2025-02-22T04:32:27Z';
+const node_5009a1a6f2e64421ae96781019935416 = () => {
+  return '2025-02-23T21:06:50Z';
 };
