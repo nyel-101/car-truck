578ddcfd1203555990a5e6c88f63faded60c376c
nyel-101 <dandevjr@gmail.com>
2025-02-26 18:11:48 +0000

    test(utils): decouple payload parser

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_374bfe1e05f1427db76d1e9f59406685 = () => {
-  return '2026-06-24T00:08:47Z';
+const node_6937c0a47d0d45d8b9b5d2fc0edbf39b = () => {
+  return '2025-02-26T18:11:48Z';
 };
