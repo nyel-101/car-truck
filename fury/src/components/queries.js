2d702866daeb7fe5a17495304110328ac245403a
nyel-101 <dandevjr@gmail.com>
2025-02-26 10:23:20 +0000

    docs(api): fix session store

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e04e8825f8949aa96f33784ece601f2 = () => {
-  return '2026-05-25T16:33:22Z';
+const node_2b112bb36ab24a87916d2019b677c7f8 = () => {
+  return '2025-02-26T10:23:20Z';
 };
