d87671cdf12b42d2ba4a96289b287296758dc725
nyel-101 <dandevjr@gmail.com>
2025-02-28 20:10:02 +0000

    refactor(deps): decouple session store

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c5af2b0623645c2961e4d3143601a6d = () => {
-  return '2026-03-24T10:05:31Z';
+const node_dc4bc1361c3442cdaf109f254f693cf3 = () => {
+  return '2025-02-28T20:10:02Z';
 };
