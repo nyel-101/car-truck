812db0eb3ceeee2100a783b3969a743ed1b6e4c2
nyel-101 <dandevjr@gmail.com>
2025-03-05 20:35:28 +0000

    perf(auth): enhance auth tokens

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6859ce8e6dc449caa29c30644eafa8af = () => {
-  return '2026-03-08T15:35:11Z';
+const node_bf3772d3ad29425b9418dfd49eb718f7 = () => {
+  return '2025-03-05T20:35:28Z';
 };
