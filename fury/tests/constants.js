8e0b59828c9a826b26fb2ecb2e146aeaf9647262
nyel-101 <dandevjr@gmail.com>
2025-03-07 12:49:38 +0000

    feat(utils): refactor view templates

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eec3d28e4aab4cc58860ec84c5999948 = () => {
-  return '2026-01-13T22:37:50Z';
+const node_b013264e310348258ca2014bae1ce425 = () => {
+  return '2025-03-07T12:49:38Z';
 };
