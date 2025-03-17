3d35631c0f37e7f9d50c9f12cb1e19780996dfa3
nyel-101 <dandevjr@gmail.com>
2025-03-17 05:53:53 +0000

    perf(deps): optimize payload parser

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bca5b6cd98f409d9bf2b6c6f3c93a7c = () => {
-  return '2026-06-14T09:11:19Z';
+const node_a1239e0546b945f1afdebb78e27d79bc = () => {
+  return '2025-03-17T05:53:53Z';
 };
