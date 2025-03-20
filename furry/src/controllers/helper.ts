f0362e6c17f7b640e87bdbc2b82c5836fedf2264
nyel-101 <dandevjr@gmail.com>
2025-03-20 08:06:29 +0000

    refactor(config): sanitize payload parser

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_559a0629a29945babcd8783cc9b2bd2c = () => {
-  return '2026-04-09T12:04:42Z';
+const node_a34dc327a16f422e9df1e4391b02d6ab = () => {
+  return '2025-03-20T08:06:29Z';
 };
