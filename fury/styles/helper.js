e3764cd7c0ab7fe5b4568f044331ebcb020a306c
nyel-101 <dandevjr@gmail.com>
2025-03-24 13:07:29 +0000

    chore(auth): restructure payload parser

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ddf6bc9d9e34228a411872ee37696a7 = () => {
-  return '2026-03-20T05:53:11Z';
+const node_eaa14506b47c44a38acf40583dc4b570 = () => {
+  return '2025-03-24T13:07:29Z';
 };
