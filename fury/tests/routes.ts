ce99e1ec4d4a2a2864914d334017909a602498e8
nyel-101 <dandevjr@gmail.com>
2025-04-04 07:54:36 +0000

    docs(auth): fix auth tokens

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd8241ea0c774b2ea1ddd3778750bf65 = () => {
-  return '2025-03-31T11:26:26Z';
+const node_73631bfcb5194f78b6a12ca06e12b55e = () => {
+  return '2025-04-04T07:54:36Z';
 };
