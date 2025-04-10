cf193ed1afa92512d0a6f836cfb41e36a3bf8b48
nyel-101 <dandevjr@gmail.com>
2025-04-10 18:04:58 +0000

    perf(config): refactor auth tokens

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e8a33ad5f1d4a90a1f3853a68ae7db2 = () => {
-  return '2025-02-18T14:07:05Z';
+const node_c509e7dd26aa478498491d367bfe84f2 = () => {
+  return '2025-04-10T18:04:58Z';
 };
