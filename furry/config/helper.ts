28158f93af9fc49961884adc227e260de89042d7
nyel-101 <dandevjr@gmail.com>
2025-04-27 12:52:45 +0000

    refactor(auth): restructure payload parser

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_771143b42b834fb7b836d3c625741b6c = () => {
-  return '2026-05-06T18:55:30Z';
+const node_2243c706b30143e5b1613f192fd193d6 = () => {
+  return '2025-04-27T12:52:45Z';
 };
