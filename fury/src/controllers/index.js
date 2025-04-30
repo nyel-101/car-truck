e10cb77a0fb06b2232454e30d2d8b82f9966eb8d
nyel-101 <dandevjr@gmail.com>
2025-04-30 20:47:17 +0000

    chore(worker): decouple session store

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7e30bfed15f40d58597bc6a0e75c36e = () => {
-  return '2025-04-24T14:23:00Z';
+const node_a1c001da5225473592b8abc151420dc1 = () => {
+  return '2025-04-30T20:47:17Z';
 };
