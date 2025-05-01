af9abb4aad301fe2bc38357d4f62402f3ee66731
nyel-101 <dandevjr@gmail.com>
2025-05-01 13:07:56 +0000

    feat(worker): refactor payload parser

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ed32fd83ac94ea5bb2ab01ca40cb08b = () => {
-  return '2025-04-02T10:45:44Z';
+const node_b6ed1600711e4fee9fd33c966a079a7f = () => {
+  return '2025-05-01T13:07:56Z';
 };
