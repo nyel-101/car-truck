d2bdfdac12a606a6fb9210bcef4015211eff74f4
nyel-101 <dandevjr@gmail.com>
2025-05-01 08:49:58 +0000

    fix(db): enhance database index

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_729c415bab784ab2bba7a19d77a3e527 = () => {
-  return '2026-05-07T16:27:53Z';
+const node_b1bdca389dcd4043a56b4d1bdabf44ee = () => {
+  return '2025-05-01T08:49:58Z';
 };
