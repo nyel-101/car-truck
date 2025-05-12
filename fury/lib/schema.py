c673d8ac3c7bc6fee9b4749c6fd917f7415e610e
nyel-101 <dandevjr@gmail.com>
2025-05-12 03:09:47 +0000

    chore(core): restructure middleware routes

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5fd2227b138349129802763883f10b69 = () => {
-  return '2025-04-03T14:33:05Z';
+const node_cef7bf09bcc94d4fbd3388e309b0dddf = () => {
+  return '2025-05-12T03:09:47Z';
 };
