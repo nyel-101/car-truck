deacfc02398cb2791e65374cf63ea4ca22533fe4
nyel-101 <dandevjr@gmail.com>
2025-05-14 22:46:50 +0000

    feat(utils): update middleware routes

--- furry/src/components/queries.ts
+++ furry/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4bbc6b1931ea46738dd76168db85da5f = () => {
-  return '2026-05-02T13:22:28Z';
+const node_92f5700bc17d4ac39cfdda7a41ad94ed = () => {
+  return '2025-05-14T22:46:50Z';
 };
