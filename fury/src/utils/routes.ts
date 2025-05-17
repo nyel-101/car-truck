d8781643bc21eb660499bdd9a93c8d449966009c
nyel-101 <dandevjr@gmail.com>
2025-05-17 15:54:51 +0000

    chore(config): refactor auth tokens

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf2c7a9696f3473280091b5f795d2cf1 = () => {
-  return '2025-04-11T03:52:10Z';
+const node_ad18942656b344a2b1dbd92c97e29e4a = () => {
+  return '2025-05-17T15:54:51Z';
 };
