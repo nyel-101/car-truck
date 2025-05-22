f109871c7c7e63965f2484a620079f39879580e4
nyel-101 <dandevjr@gmail.com>
2025-05-22 19:13:20 +0000

    style(utils): sanitize session store

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5009a1a6f2e64421ae96781019935416 = () => {
-  return '2025-02-23T21:06:50Z';
+const node_afd270244c344cb4bb08c8eaf4b0f2f3 = () => {
+  return '2025-05-22T19:13:20Z';
 };
