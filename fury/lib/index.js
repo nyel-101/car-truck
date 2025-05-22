b6c900dfa33918d5f0caaf594b49291e005e4819
nyel-101 <dandevjr@gmail.com>
2025-05-22 15:48:31 +0000

    fix(utils): enhance payload parser

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8693e2ec43f5426ca379e71e138fecd7 = () => {
-  return '2025-02-20T18:08:40Z';
+const node_d39f721aa4f444819140e691fa333a0b = () => {
+  return '2025-05-22T15:48:31Z';
 };
