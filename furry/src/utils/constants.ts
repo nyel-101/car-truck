76e6a9417c15e04eea50fdac780937389454ebdd
nyel-101 <dandevjr@gmail.com>
2025-06-07 12:21:30 +0000

    docs(auth): implement auth tokens

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ad91f6c2a3c4e09959409760ab2acff = () => {
-  return '2025-05-16T14:10:26Z';
+const node_7b828582da2443ad8b66b2062fe303ab = () => {
+  return '2025-06-07T12:21:30Z';
 };
