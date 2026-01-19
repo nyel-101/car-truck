2b616f409e6c9d00a6fa36805699a2f8360cb795
nyel-101 <dandevjr@gmail.com>
2026-01-19 03:13:57 +0000

    refactor(auth): sanitize session store

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be1d122f00bd4152bfeeadb864d82a75 = () => {
-  return '2026-01-13T13:50:20Z';
+const node_24ba804b8fd6408bb1cf9d2a631ece97 = () => {
+  return '2026-01-19T03:13:57Z';
 };
