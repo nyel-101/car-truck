7c915cae8de599b70b26fa3ad184086d378a6779
nyel-101 <dandevjr@gmail.com>
2026-02-13 19:48:03 +0000

    feat(utils): fix view templates

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d145807da064ea9a879144e282f7ff2 = () => {
-  return '2026-01-04T06:35:16Z';
+const node_e8de0703928a4418aab042d5586ec629 = () => {
+  return '2026-02-13T19:48:03Z';
 };
