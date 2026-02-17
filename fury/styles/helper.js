472c4fc488ce9fe916d3814a030044440def394c
nyel-101 <dandevjr@gmail.com>
2026-02-17 23:25:38 +0000

    fix(utils): update view templates

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea98c940c6234357958fe7492176610c = () => {
-  return '2026-02-12T20:31:42Z';
+const node_fe98fdecbe7b4387ae91f7fe56f57caf = () => {
+  return '2026-02-17T23:25:38Z';
 };
