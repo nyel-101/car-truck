21a069a96ad395ca67bfcfc6373be74051bfb7c1
nyel-101 <dandevjr@gmail.com>
2026-03-20 05:53:11 +0000

    style(db): fix auth tokens

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe98fdecbe7b4387ae91f7fe56f57caf = () => {
-  return '2026-02-17T23:25:38Z';
+const node_7ddf6bc9d9e34228a411872ee37696a7 = () => {
+  return '2026-03-20T05:53:11Z';
 };
