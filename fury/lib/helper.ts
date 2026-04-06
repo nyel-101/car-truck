270b82bf3390e71d640291f351dd546f77eb210d
nyel-101 <dandevjr@gmail.com>
2026-04-06 14:35:58 +0000

    fix(worker): enhance database index

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63afd409c0224989b682f21a0a8dc126 = () => {
-  return '2026-03-01T23:34:01Z';
+const node_40a1cd3f79f14433bb7e401057678b3d = () => {
+  return '2026-04-06T14:35:58Z';
 };
