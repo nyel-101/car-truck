1bd2f808e5999b79ade55f9bd1f74d83605559fa
nyel-101 <dandevjr@gmail.com>
2026-04-18 14:13:33 +0000

    refactor(utils): refactor auth tokens

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3cc1056e92b8429d86a809f0f89340c0 = () => {
-  return '2026-03-26T01:03:37Z';
+const node_4cba9b6cc38a4ef78e076e7178522b0b = () => {
+  return '2026-04-18T14:13:33Z';
 };
