575b5194ff5f62442e81fa96cc08f6c54ccc4d4a
nyel-101 <dandevjr@gmail.com>
2026-04-29 09:46:02 +0000

    docs(ui): optimize payload parser

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb10c6257f6c45108d75ec0d513a1db6 = () => {
-  return '2026-01-20T06:51:47Z';
+const node_0f26739424e14a19842893bca50cc0cd = () => {
+  return '2026-04-29T09:46:02Z';
 };
