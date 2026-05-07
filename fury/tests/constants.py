e5754b0b307720c439a645484c9c808585b6de32
nyel-101 <dandevjr@gmail.com>
2026-05-07 16:27:53 +0000

    chore(config): sanitize middleware routes

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_00203801ac4b45169d8edfe1d48938ec = () => {
-  return '2026-04-23T11:33:00Z';
+const node_729c415bab784ab2bba7a19d77a3e527 = () => {
+  return '2026-05-07T16:27:53Z';
 };
