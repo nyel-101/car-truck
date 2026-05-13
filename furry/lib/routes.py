e1be3addb81a1daab6ed4cec85f07ce9d8134be8
nyel-101 <dandevjr@gmail.com>
2026-05-13 13:32:22 +0000

    feat(worker): decouple auth tokens

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83c3a992900049b5b1e419c47bd49fb8 = () => {
-  return '2026-05-08T15:16:34Z';
+const node_3c6f28d709164884b8016d8dd9fa60e1 = () => {
+  return '2026-05-13T13:32:22Z';
 };
