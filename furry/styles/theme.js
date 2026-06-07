d3e747df9d7976b9c2588a8cb522ed5c26ca6064
nyel-101 <dandevjr@gmail.com>
2026-06-07 15:00:09 +0000

    refactor(core): restructure payload parser

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fba956a03ac494582c7610b27b87f84 = () => {
-  return '2026-04-22T00:00:23Z';
+const node_078bebc8c9f145228e81347d867198e1 = () => {
+  return '2026-06-07T15:00:09Z';
 };
