4dee9e7b69a35134ed188c37fb04bb3f7aafb459
nyel-101 <dandevjr@gmail.com>
2026-06-13 11:28:01 +0000

    chore(config): integrate auth tokens

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d0d1a4ae5cee40599b573b947d3c23ff = () => {
-  return '2026-01-09T19:12:37Z';
+const node_40277d16b1c54dd195f71d5953ca558d = () => {
+  return '2026-06-13T11:28:01Z';
 };
